f34713f53f2b0b73c2f3d39e7330150dcd7f4f08
André Marçais <andre@marcais.net>
2025-04-04 08:58:18 +0200

    Create users and groups in DataFaker

--- png/src/main/java/fr/univtln/m1im/png/generation/DataFaker.java
+++ png/src/main/java/fr/univtln/m1im/png/generation/DataFaker.java
@@ -1,27 +1,44 @@
 package fr.univtln.m1im.png.generation;
 
-import static java.text.Normalizer.normalize;
-
-import java.text.Normalizer.Form;
-import java.time.LocalDate;
-import java.util.stream.Collectors;
-
 import fr.univtln.m1im.png.Utils;
 import fr.univtln.m1im.png.model.Etudiant;
-import fr.univtln.m1im.png.model.Groupe;
-import fr.univtln.m1im.png.model.Module;
-import fr.univtln.m1im.png.model.Utilisateur;
-import jakarta.persistence.Persistence;
+import jakarta.persistence.EntityManagerFactory;
 
 public class DataFaker {
     public static void main(String[] args) {
-        final var emf = Utils.getEntityManagerFactory();
+        Utils.initconnection("postgres", "mysecretpassword");
+        try (final var emf = Utils.getEntityManagerFactory()) {
+            fakeData(emf);
+        }
+    }
+
+    public static void fakeData(EntityManagerFactory emf) {
         final var rand = new java.util.Random(123);
         final var faker = new com.github.javafaker.Faker(java.util.Locale.FRANCE, rand);
 
-        //Etudiant etudiant = FakeUserBuilder.of(Etudiant.builder(), faker, rand).build();
-        var etudiant = FakeUser.with(faker, rand).withStudentEmail().configure(Etudiant.builder()).build();
+        var groups = GroupFaker.with(rand).createGroups();
+        try (var em = emf.createEntityManager()) {
+            try {
+                em.getTransaction().begin();
 
-        var module = Module.builder().code("M1").nom("Module1").description("Description1").nbHeuresCM(10).nbHeuresTD(20).nbHeuresTP(30).build();
+                for (var group : groups) {
+                    em.persist(group);
+                    if (group.getSousGroupes().isEmpty()) {
+                        for (int i = 0; i < 1000; ++i) {
+                            var etudiant = FakeUser.with(faker, rand).withStudentEmail()
+                                .configure(Etudiant.builder()).build();
+                            em.persist(etudiant);
+                        }
+                    }
+                }
+
+                em.getTransaction().commit();
+            } catch (Throwable e) {
+                em.getTransaction().rollback();
+                throw e;
+            }
+        }
+
+        //var module = Module.builder().code("M1").nom("Module1").description("Description1").nbHeuresCM(10).nbHeuresTD(20).nbHeuresTP(30).build();
     }
 }
