e866b34286853e69ecac816c88c74a05e0400ccd
tombartos <tombartos83@gmail.com>
2025-04-06 12:45:10 +0200

    Revert "Merge branch 'andre' into Tom"
    This reverts commit a786a03bb0defdf7d6daf9f29e504145e81e3100, reversing
    changes made to 7c71986a42ac281eb83465278372864680e9e384.

--- png/src/main/java/fr/univtln/m1im/png/model/Groupe.java
+++ png/src/main/java/fr/univtln/m1im/png/model/Groupe.java
@@ -29,7 +29,7 @@ import java.util.List;
 @ToString
 @NamedQueries({
     @NamedQuery(
-        name = "Groupe.getAll",
+        name = "Groupe.getAll", 
         query = "SELECT g FROM Groupe g"),
     @NamedQuery(
         name = "Groupe.getAllDTO",
